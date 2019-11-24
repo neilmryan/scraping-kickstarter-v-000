6ce128087828f341bb8d609edce30001ae317eb9
Neil Ryan <neilmarkoliverryan@gmail.com>
2019-11-24 08:51:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,7 +18,8 @@ def create_project_hash
       projects[project.css("h2.bbcard_name strong a").text] = {
         :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
         :description => project.css("p.bbcard_blurb").text,
-        :location => project.css("span.location-name").text
+        :location => project.css("span.location-name").text,
+        :percent_funded => project.css("li.first.funded strong").text.gsub("%", "").to_i
       }
     
     
