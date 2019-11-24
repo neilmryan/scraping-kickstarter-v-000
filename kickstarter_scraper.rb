b607902d23f0b9eacaeef0877ec65e811b18fe31
Neil Ryan <neilmarkoliverryan@gmail.com>
2019-11-24 08:44:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
+# percent funded: project.css("li.first.funded strong").text
 
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
