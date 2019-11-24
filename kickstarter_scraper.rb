47d58fd30a2b47352c0c20ac8883f059a0b77125
Neil Ryan <neilmarkoliverryan@gmail.com>
2019-11-24 08:27:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,8 @@
 require 'nokogiri'
 require 'pry'
 
-html = File.read("fixtures/kickstarter.html")
 
-kickstarter = Nokogiri::HTML(html)
+# projects: kickstarter.css("li.project.grid_4")
 
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
